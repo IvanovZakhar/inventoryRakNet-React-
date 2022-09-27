6e01dc34729baf7f3569d129b7425b6ca5083184
3axap <uvanov977777@gmail.com>
2022-09-27 21:09:40 +0300

    ready-made functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,22 @@
-import { Component } from 'react';
-import './app.css';
 import Inv from '../inv/inv';
+import './app.css';
 
-class App extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            data: [
+
+const App = () => {
+
+         const data = [
                 {weapon:'mp-5', ammunition: '800'},
                 {weapon:'m4', ammunition: '3000'},
-                {weapon:'deagle', ammunition: '15000'},
-            ]
-        }
-    }
+                {weapon:'deagle', ammunition: '15000'}]
 
-    render(){
+
+
         return(
             <div className="app">
                 <Inv/>
             </div>
         )
-    }
+    
 }
 
 
--- src/components/inv/inv.js
+++ src/components/inv/inv.js
@@ -1,38 +1,98 @@
+import { createElement, useState } from 'react';
+
 import './inv.css'
-import { Component } from 'react';
 
-class Inv extends Component{
-    constructor(props){
-        super(props)  
+
+const Inv = () => {
+  const [inv, setInv] = useState(
+[    {id: 1, item: 'https://www.iconninja.com/files/477/796/985/mp5-gun-icon.png'},
+    {id: 2, item: null},
+    {id: 3, item: null},
+    {id: 4, item: null},
+    {id: 5, item: null},
+    {id: 6, item: null},
+    {id: 7, item: null},
+    {id: 8, item: null},
+    {id: 9, item: null},
+    {id: 10, item: null},
+    {id: 11, item: null},
+    {id: 12, item: null},
+    {id: 13, item: null},
+    {id: 14, item: null},
+    {id: 15, item: null},
+    {id: 16, item: null}]
+  );
+
+  const[currentCard, setCurrentCard] = useState(null);
+
+
+  function dragStartHandler (e, card){
+    e.target.tagName === 'IMG' ? setCurrentCard(card) : e.preventDefault();
+    
+    
+    
+  }
+
+  function dragEndHandler(e, card) {
+    e.preventDefault(); 
+ 
+  }
+
+  function dragOverHandler(e, card) {
+
+    e.preventDefault();
+ 
+    
+  }
+
+  function dropHandler(e, card){
+    e.preventDefault();
+    setInv(inv.sort(sortCards).map(c => {
+      if(c.id === card.id){
+        return{...c, item: currentCard.item}
+      }
+      if(c.id === currentCard.id){
+        return{...c, item: card.item}
+      }
+      return c
+    }))
+  }
+
+  const sortCards = (a, b) => {
+    if(a.id > b.id){
+      return 1
+    }else{ 
+      return -1
     }
+  }
+  
 
-    render() {
         return(
-            <div class="row">
-            <div class="placeholder">
-              <div class="item" draggable="true">
-                <img src="https://www.iconninja.com/files/477/796/985/mp5-gun-icon.png" alt="" /> 
-              </div>
+          <div className="inv">
+            <div className="row">
+              {inv.map(card => 
+                <div 
+                  key={card.id} 
+                  className="placeholder"
+                  draggable={true}
+                  onDragStart={(e) => dragStartHandler(e, card)}
+                  onDragLeave={(e) => dragEndHandler(e, card)}
+                  onDragEnd={(e) => dragEndHandler(e, card)}
+                  onDragOver={(e) => dragOverHandler(e, card)}
+                  onDrop={(e) => dropHandler(e, card)}>
+                  {card.item ? <img src={card.item} alt="item"/> : null }
+                </div>
+                )}
             </div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div> 
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
-            <div class="placeholder"></div>
           </div>
 
+
         )
-    }
+    
+}
+
+const elem = (elem) => {
+
 }
 
 export default Inv;
